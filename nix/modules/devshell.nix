4f9f19832fc10a9ebca5952d5ec9fbf54a8231df
gignsky <gig@gignsky.com>
2025-05-05 22:12:14 -0400

    added rustup template

---
+++ nix/modules/devshell.nix
@@ -0,0 +1,31 @@
+{ inputs, ... }:
+{
+  perSystem = { config, self', pkgs, lib, ... }: {
+    devShells.default = pkgs.mkShell {
+      name = "playarr-shell";
+      inputsFrom = [
+        self'.devShells.rust
+        config.treefmt.build.devShell
+        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+      ];
+      packages = with pkgs; [
+        just
+        nixd # Nix language server
+        bacon
+        config.process-compose.cargo-doc-live.outputs.package
+        nil
+        lolcat
+        wslu
+        cargo-generate
+
+        # dotfiles programs
+        inputs.dotfiles.packages.${system}.quick-results
+        inputs.dotfiles.packages.${system}.upjust
+        inputs.dotfiles.packages.${system}.cargo-update
+      ];
+      shellHook = ''
+        echo "welcome to the rust development environment" | ${pkgs.cowsay}/bin/cowsay
+      '';
+    };
+  };
+}
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+// ARGS Parser
+use clap::Parser;
+// // TUI Stuff
+// use color_eyre::Result;
+// use crossterm::event::{self, Event};
+// use ratatui::{DefaultTerminal, Frame};
+
+#[derive(Parser, Debug)]
+#[clap(author = "Maxwell Rupp", version, about)]
+/// Application configuration
+struct Args {
+    /// whether to be verbose
+    #[arg(short = 'v')]
+    verbose: bool,
+
+    /// an optional name to greet
+    #[arg()]
+    name: Option<String>,
+}
+
+// Non-TUI Stuff
+fn main() {
+    let args = Args::parse();
+    if args.verbose {
+        println!("DEBUG {args:?}");
+    }
+    println!(
+        "Hello {} (from playarr)!",
+        args.name.unwrap_or("world".to_string())
+    );
+}
+
+// // TUI Stuff
+// fn main() -> Result<()> {
+//     color_eyre::install()?;
+//     let terminal = ratatui::init();
+//     let result = run(terminal);
+//     ratatui::restore();
+//     result
+// }
+
+// fn run(mut terminal: DefaultTerminal) -> Result<()> {
+//     loop {
+//         terminal.draw(render)?;
+//         if matches!(event::read()?, Event::Key(_)) {
+//             break Ok(());
+//         }
+//     }
+// }
+
+// fn render(frame: &mut Frame) {
+//     let args = Args::parse();
+//     if args.verbose {
+//         println!("DEBUG {args:?}");
+//     }
+//     let welcome_message = format!(
+//         "Hello {} (from playarr)!",
+//         args.name.unwrap_or("world".to_string())
+//     );
+//     frame.render_widget(welcome_message, frame.area());
+// }
